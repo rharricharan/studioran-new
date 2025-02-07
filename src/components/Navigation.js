f54dfc7df4f28d6bfca47636f84a14a389f43cec
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2025-02-07 17:39:15 -0500

    starting nav file

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect } from "react";
+import gsap from "gsap";
+import SplitType from "split-type";
+
+export default function Navigation() {
+    return (
+        <div>
+
+        </div>
+    )
+};
